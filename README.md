6d103e0b9f44a67415b07ca687095cb811f5fc68
Meesohy <124698885+Meesohy@users.noreply.github.com>
2023-03-30 14:50:16 +0000

    first try

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Assignment2b
+
+
+
---
+++ main.py
@@ -0,0 +1,10 @@
+# Assignment2b
+value = input ("Welcome! Let's calculate the area and circumference of a circle!") 
+diameter= float (input ("Please enter the diameter of the circle:"))
+diameter= float (input ("if error detected "))
+radius= float(diameter/2)
+print (3.14*radius*radius)
+print (2*3.14*radius)
+#print("You entered "+value+" and its area is "+value*pi*radius*radius)
+#radius= diameter/2
+#print("You entered "+value+" and its area is {"value*pi*radius*radius}")
