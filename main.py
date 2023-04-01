1f22782fbeea1dac99f3a6165f14313c95d5024e
Meesohy <124698885+Meesohy@users.noreply.github.com>
2023-04-01 15:41:13 +0000

    final

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Assignment2b
-value = input ("Welcome! Let's calculate the area and circumference of a circle!") 
-diameter= float (input ("Please enter the diameter of the circle:"))
-diameter= float (input ("if error detected "))
-radius= float(diameter/2)
+value = input ("Welcome! Let's calculate the area and circumference of a circle! Click enter!")
+value = input ("Note that top is the area and bottom is the circumference! Click enter to proceed!")
+diameter= input ("Please enter the diameter of the circle:")
+radius= float(diameter)/2
+#if diameter.isnumeric():
+    #print ("Error detected! Please re-input a proper value!")
+#else:
+    #print("Error detected!")
 print (3.14*radius*radius)
 print (2*3.14*radius)
-#print("You entered "+value+" and its area is "+value*pi*radius*radius)
-#radius= diameter/2
-#print("You entered "+value+" and its area is {"value*pi*radius*radius}")
+value = input ("Success!")
